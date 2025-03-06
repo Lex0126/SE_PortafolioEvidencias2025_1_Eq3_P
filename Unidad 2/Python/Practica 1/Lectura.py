c46fe18b9e6a897bf1f5518c83fe98a9c8c1473d
lex0126 <lorenzo.s.lexiss.18@gamil.com>
2025-03-05 18:40:01 -0600

    cambios

--- Unidad 2/Python/Practica 1/Lectura.py
+++ Unidad 2/Python/Practica 1/Lectura.py
@@ -18,13 +18,13 @@ class Lectura:
     def calcular_desviacion(self, matriz):
         df = pd.DataFrame(matriz, columns=["V1", "V2", "V3", "V4", "V5", "V6"])
 
-        # Desviación estándar por fila
+        # Desviacion estandar por fila
         df['Desviacion_Fila'] = df.std(axis=1, ddof=1)
 
-        # Desviación estándar por columna
+        # Desviacion estandar por columna
         desviacion_columnas = df.std(axis=0, ddof=1).tolist()
 
-        # Añadir desviación estándar por columna como fila
+        # Añadir desviacion estandar por columna como fila
         desviacion_fila = pd.DataFrame([desviacion_columnas], columns=[f'Desviacion_{col}' for col in df.columns])
         df = pd.concat([desviacion_fila, df], ignore_index=True)
 
